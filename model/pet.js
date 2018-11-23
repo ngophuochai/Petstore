4b1887ff83775948a5940831a6ccaf78c32589d4
ngophuochai <ngophuochai1512138@gmail.com>
2018-11-23 22:41:32 +0700

    change Kitty04 -> Kitty00004

--- model/pet.js
+++ model/pet.js
@@ -2,7 +2,7 @@ let ListData = [
     {id: 1, name: 'Kitty01', status: 'available'},
     {id: 2, name: 'Kitty02', status: 'available'},
     {id: 3, name: 'Kitty03', status: 'available'},
-    {id: 4, name: 'Kitty04', status: 'available'},
+    {id: 4, name: 'Kitty00004', status: 'available'},
     {id: 5, name: 'Kitty05', status: 'available'},
     {id: 6, name: 'Kitty06', status: 'available'},
     {id: 7, name: 'Kitty07', status: 'available'},
